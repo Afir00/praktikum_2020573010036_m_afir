af08b04d76c123bf52f5b2eeb4a49eb9dd964d3e
afir00 <afir832@gmail.com>
2021-09-17 21:37:14 +0700

    Menambahkan file coba 11.php

---
+++ praktikum_2_2020573010036_m_afir/coba 11.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+       $val1 ="Jika Sedang Belajar, ";
+       $val2 ="Harap Jangan ribut" ;       
+       echo "Menggabungkan 2 string Val 1 dan Val 2 : ";
+       echo $val1 .  $val2;
+       echo "<br>";
+    ?>
+</body>
+</html>
