40521a7a7a24827ca5b08d6965335ce09583f5ef
afir00 <afir832@gmail.com>
2021-11-01 08:48:32 +0700

    Menambahkan file coba 4a.php

--- praktikum_6_2020573010036_m_afir/coba 4a.php
+++ praktikum_6_2020573010036_m_afir/coba 4a.php
@@ -20,14 +20,3 @@ coba4a
     </form>
 </body>
 </html>
-
-coba4b
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
